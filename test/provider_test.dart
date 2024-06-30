3b9d7ff1cf784c5ae25d83882b1a97428289eb34
Ayan <ayandas9531@gmail.com>
2024-06-30 13:33:48 +0530

    Testing nuggets ongoing

--- test/provider_test.dart
+++ test/provider_test.dart
@@ -1,6 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nugget_berg/state/auth/providers/access_token.dart';
+import 'package:nugget_berg/state/nuggets/models/nugget.dart';
+import 'package:nugget_berg/state/nuggets/providers/nugget_by_video_id.dart';
+import 'package:nugget_berg/state/nuggets/providers/nuggets.dart';
 import 'package:nugget_berg/state/videos/models/video.dart';
+import 'package:nugget_berg/state/videos/provider/next_page_token.dart';
 import 'package:nugget_berg/state/videos/provider/video_repository.dart';
 import 'package:nugget_berg/state/videos/provider/videos.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -30,7 +34,12 @@ ProviderContainer createContainer({
 }
 
 // Mocks
-class MockVideoRepository extends Mock implements VideoRepository {}
+class MockVideoRepository extends Mock implements VideoRepository {
+  @override
+  Future<Nugget?> getNuggetFromVideo({required Video video}) async {
+    return Nugget(video: video, points: []);
+  }
+}
 
 void main() {
   late MockVideoRepository? mockVideoRepository;
@@ -39,7 +48,14 @@ void main() {
     mockVideoRepository = MockVideoRepository();
   });
 
-  final videosFromPage1 = [
+  const videosFromPage1 = [
+    Video(
+        id: '0',
+        channelId: '',
+        title: 'video 1',
+        description: 'video 1 desc',
+        thumbnail: '',
+        channelTitle: ''),
     Video(
         id: '1',
         channelId: '',
@@ -69,7 +85,7 @@ void main() {
         thumbnail: '',
         channelTitle: ''),
   ];
-  final videosFromPage2 = [
+  const videosFromPage2 = [
     Video(
         id: '5',
         channelId: '',
@@ -100,10 +116,26 @@ void main() {
         channelTitle: ''),
   ];
 
+  final nuggetsFromPage1 =
+      videosFromPage1.map((e) => Nugget(video: e, points: const [])).toList();
+  final nuggetsFromPage2 =
+      videosFromPage2.map((e) => Nugget(video: e, points: const [])).toList();
+
   void videoRepoReturnsVideos() {
-    when(() => mockVideoRepository!.getVideos(accessToken: 'token'))
-        .thenAnswer((annotation) async {
-      return videosFromPage1;
+    when(() => mockVideoRepository!.getVideos(
+          accessToken: 'token',
+        )).thenAnswer((annotation) async {
+      return VideosAndNextPageToken(videos: videosFromPage1, nextPageToken: '2');
+    });
+    when(() => mockVideoRepository!.getVideos(
+        accessToken: 'token',
+        nextPageToken: '1')).thenAnswer((annotation) async {
+      return VideosAndNextPageToken(videos: videosFromPage1, nextPageToken: '2');
+    });
+    when(() => mockVideoRepository!.getVideos(
+        accessToken: 'token',
+        nextPageToken: '2')).thenAnswer((annotation) async {
+      return VideosAndNextPageToken(videos: videosFromPage2, nextPageToken: '3');
     });
   }
 
@@ -116,19 +148,95 @@ void main() {
       // video repo getVideos
       videoRepositoryProvider.overrideWith((ref) {
         return mockVideoRepository!;
-      })
+      }),
     ]);
   }
 
-  test('videoProvider updates using VideoRepository', () async {
-    videoRepoReturnsVideos();
-    final container = createContainerWithoverrides();
-    await container.read(videoProviderProvider.notifier).updateList();
-    final videos = container.read(videoProviderProvider);
-    expect(videos, videosFromPage1);
+  group('All mocks work as expected', () {
+    test('Testing providers without any method run', () async {
+      videoRepoReturnsVideos();
+      final container = createContainerWithoverrides();
+      await container.read(videoProviderProvider.notifier).updateList();
+      final videos = container.read(videoProviderProvider);
+      final accessToken = await container.read(accessTokenProvider.future);
+      final nextPageToken = container.read(nextPageTokenProvider);
+      expect(videos, videosFromPage1);
+      expect(accessToken, 'token');
+      expect(nextPageToken, '2');
+    });
+
+    test('Video repository returns videos based on accesstoken', () async {
+      videoRepoReturnsVideos();
+      final container = createContainerWithoverrides();
+
+      final video1 = await container
+          .read(videoRepositoryProvider)
+          .getVideos(accessToken: 'token', nextPageToken: '1');
+      expect(video1, videosFromPage1);
+      final video2 = await container
+          .read(videoRepositoryProvider)
+          .getVideos(accessToken: 'token', nextPageToken: '2');
+      expect(video2, videosFromPage2);
+    });
+
+    test('videoProvider updates using VideoRepository', () async {
+      videoRepoReturnsVideos();
+      final container = createContainerWithoverrides();
+      container.read(nextPageTokenProvider.notifier);
+      // final accessToken = await container.read(accessTokenProvider.future);
+      // final nextPageToken = container.read(nextPageTokenProvider);
+
+      await container.read(videoProviderProvider.notifier).updateList();
+      final videos = container.read(videoProviderProvider);
+      print(videos);
+      expect(videos, videosFromPage1);
+    });
   });
 
-  test('Videos list based on page token', (){
-    videoRepoReturnsVideos();
+  group('Nuggets provider working', () {
+    test('Nugget by video provider returns', () async {
+      videoRepoReturnsVideos();
+      final container = createContainerWithoverrides();
+      final nugget = await container
+          .read(NuggetByVideoProvider(videosFromPage1.first).future);
+      expect(nugget, Nugget(video: videosFromPage1.first, points: const []));
+    });
+
+    test('Nugget provider works', () async {
+      videoRepoReturnsVideos();
+      final container = createContainerWithoverrides();
+      await container.read(videoProviderProvider.notifier).updateList();
+      final nuggetsSub = container.listen(nuggetsProvider, (_, __) {});
+      final nuggetNotifier = container.read(nuggetsProvider.notifier);
+      // Empty nuggets without loadVideos()
+      expect(nuggetsSub.read(), []);
+
+      // loadVideos() called should first 2 nuggets
+      await nuggetNotifier.loadNuggets();
+
+      // Nuggets from page one
+      await Future.delayed(const Duration(milliseconds: 500));
+
+      expect(nuggetsSub.read(), [nuggetsFromPage1.first, nuggetsFromPage1[1]]);
+
+      await nuggetNotifier.getNextNuggetOrRemoveVideo(currentIndex: 1);
+
+      expect(nuggetsSub.read(), nuggetsFromPage1.sublist(0, 3));
+
+      await nuggetNotifier.getNextNuggetOrRemoveVideo(currentIndex: 2);
+
+      expect(nuggetsSub.read(), nuggetsFromPage1.sublist(0, 4));
+
+      await nuggetNotifier.getNextNuggetOrRemoveVideo(currentIndex: 3);
+
+      expect(nuggetsSub.read(), nuggetsFromPage1.sublist(0, 5));
+
+      // await nuggetNotifier.getNextNuggetOrRemoveVideo(currentIndex: 4);
+      // print(nuggetsSub.read());
+      // expect(nuggetsSub.read(), [...nuggetsFromPage1, nuggetsFromPage2.first]);
+
+      // Disposing container
+      container.dispose();
+    });
   });
 }
